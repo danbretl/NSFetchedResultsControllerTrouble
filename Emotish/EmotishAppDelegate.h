06f2aaca986579e04ba6badc0224f6f88d14f2ff
Dan Bretl <danbretl@gmail.com>
2012-01-25 14:42:23 -0500

    Initial Commit

---
+++ Emotish/EmotishAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  EmotishAppDelegate.h
+//  Emotish
+//
+//  Created by Dan Bretl on 1/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmotishAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Emotish/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Emotish
+//
+//  Created by Dan Bretl on 1/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EmotishAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EmotishAppDelegate class]));
+    }
+}
