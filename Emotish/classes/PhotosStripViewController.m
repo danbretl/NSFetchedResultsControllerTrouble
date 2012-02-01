5e980a1285c8be39a1a74f0f55ebb443f4095138
Dan Bretl <danbretl@gmail.com>
2012-02-01 17:09:07 -0500

    Line break

--- Emotish/classes/PhotosStripViewController.m
+++ Emotish/classes/PhotosStripViewController.m
@@ -142,8 +142,7 @@
     // e.g. self.myOutlet = nil;
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
