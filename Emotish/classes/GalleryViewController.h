635e03c02b350b2a2b517e15d2001926c95e1e1c
Dan Bretl <danbretl@gmail.com>
2012-03-21 21:37:17 -0400

    Code formatting adjustments

--- Emotish/classes/GalleryViewController.h
+++ Emotish/classes/GalleryViewController.h
@@ -17,16 +17,23 @@ typedef enum {
 
 @interface GalleryViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate, WebGetPhotosDelegate>
 
+// Gallery mode, data, sort descriptors, etc
+@property (nonatomic) GalleryMode galleryMode;
+@property (strong, nonatomic) CoreDataManager * coreDataManager;
+@property (strong, nonatomic) NSFetchedResultsController * fetchedResultsController;
+@property (nonatomic, strong, readonly) NSSortDescriptor * sortDescriptorAlphabetical;
+@property (nonatomic, strong, readonly) NSSortDescriptor * sortDescriptorRecent;
+- (NSSortDescriptor *) sortDescriptorForGalleryMode:(GalleryMode)galleryMode;
+
+// Views
 @property (strong, nonatomic) IBOutlet UITableView * feelingsTableView;
 @property (unsafe_unretained, nonatomic) IBOutlet UIButton * addPhotoButton;
 @property (unsafe_unretained, nonatomic) IBOutlet UIButton * pullPhotosButton;
 @property (unsafe_unretained, nonatomic) IBOutlet UIButton *toggleModesButton;
 - (IBAction) buttonTouched:(UIButton *)button;
-@property (strong, nonatomic) CoreDataManager * coreDataManager;
-@property (strong, nonatomic) NSFetchedResultsController * fetchedResultsController;
-
 - (void)tableView:(UITableView *)tableView configureCell:(UITableViewCell *)feelingCell atIndexPath:(NSIndexPath *)indexPath;
 
+// Web related properties and methods
 - (void) getPhotosFromServer;
 - (void) getPhotosFromServerWithLimit:(NSNumber *)limit;
 - (void) getPhotosFromServerForFeeling:(Feeling *)feeling;
@@ -37,10 +44,5 @@ typedef enum {
 @property (nonatomic, strong, readonly) NSArray * addPhotoFeelingWords;
 @property (nonatomic, readonly) int addPhotoFeelingWordsNextIndex;
 @property (nonatomic, readonly) int peekAtNextAddPhotoFeelingWordsIndex;
-
-@property (nonatomic) GalleryMode galleryMode;
-@property (nonatomic, strong, readonly) NSSortDescriptor * sortDescriptorAlphabetical;
-@property (nonatomic, strong, readonly) NSSortDescriptor * sortDescriptorRecent;
-- (NSSortDescriptor *) sortDescriptorForGalleryMode:(GalleryMode)galleryMode;
 
 @end
